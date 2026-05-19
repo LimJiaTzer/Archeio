5e3ab7e0cefeed6326b0314fe091d0d9508ac3a0
Nathaniel-Lim <Cloud9RMJ@gmail.com>
2026-05-19 16:43:01 +0800

    I wanna quit

---
+++ frontend/src/pages/Features.jsx
@@ -0,0 +1,7 @@
+export default function Features() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-stone-100">
+      <h1 className="text-5xl font-bold">There are no features<br />We are scammers</h1>
+    </div>
+  );
+}
