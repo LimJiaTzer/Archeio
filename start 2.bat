a07baf0322e7b285efde42568e79d40a4632c78d
LimJiaTzer <limjiatzer@gmail.com>
2026-07-25 19:10:40 +0800

    remove the extra bash files

--- start 2.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Windows: double-click this file to install and start Archeio.
-cd /d "%~dp0"
-npm start
--- start 2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Linux: run ./start.sh to install and start Archeio.
-cd "$(dirname "$0")" || exit 1
-npm start
